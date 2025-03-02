d83b48a3d913fc237bece76fb80b92d0db7ad440
g3rar2 <113952421+g3rar2@users.noreply.github.com>
2025-03-01 19:37:43 -0600

    added variables and methods

--- OperacionesApp/ViewModels/OperacionesViewModel.cs
+++ OperacionesApp/ViewModels/OperacionesViewModel.cs
@@ -8,5 +8,33 @@ namespace OperacionesApp.ViewModels
 {
     class OperacionesViewModel
     {
+
+        public double ladoCuadrado { get; set; }
+        public double radioCirculo { get; set; }
+        public double baseRectangulo { get; set; }
+        public double alturaRectangulo { get; set; }
+
+        public double areaCuadrado(double ladoCuadrado)
+        {
+            ladoCuadrado=this.ladoCuadrado;
+
+            return ladoCuadrado * ladoCuadrado;
+        }
+
+        public double areaCirculo(double radioCirculo)
+        {
+            radioCirculo = this.radioCirculo;
+
+            return 3.14*radioCirculo*radioCirculo;
+        }
+
+        public double areaRectangulo(double baseRectangulo, double alturaRectangulo)
+        {
+            baseRectangulo = this.baseRectangulo;
+            alturaRectangulo = this.alturaRectangulo;
+
+            return baseRectangulo*alturaRectangulo;
+        }
+
     }
 }
